15e902af083fccb4d8d8fe12257209d602688ef4
mekena McGrew <mekenametcalf@mekenas-MacBook-Pro.local>
2015-06-15 12:58:56 -0700

    Initial Commit

---
+++ Fock Basis Binary/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Fock Basis Binary
+//
+//  Created by mekena McGrew on 6/15/15.
+//  Copyright (c) 2015 Mekena Metcalf. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
