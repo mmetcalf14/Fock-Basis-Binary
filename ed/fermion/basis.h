a412b75a9e65e6f075a914d9a0078bcfd9d4584c
ChenYen Lai <chengyanlai@gmail.com>
2015-07-01 22:54:37 -0700

    interaction hamiltonian[chenyen]

--- ed/fermion/basis.h
+++ ed/fermion/basis.h
@@ -1,6 +1,7 @@
 #ifndef __BASIS_H__
 #define __BASIS_H__
 #include <vector>
+#include <numeric>
 
 class FermionBasis {
 private:
@@ -24,6 +25,8 @@ public:
   void PrintIndices2()const;
   inline size_t getL()const{return lattice_points;};
   inline size_t getNSpecies()const{return number_species;};
+  inline size_t getTotalN()const{return std::accumulate(NSpecies.begin(), NSpecies.end(), 0);};
+  inline size_t getN( const size_t species_id )const{return NSpecies.at(species_id);};
   inline uint64_t getTotalHilbertSpace()const{return TotalHilbertSpace;};
   inline uint64_t getEachHilbertSpace(const size_t id)const{return HilbertSpace[id];};
   inline size_t getIndices2(const size_t &idx0, const size_t &idx1)const{
