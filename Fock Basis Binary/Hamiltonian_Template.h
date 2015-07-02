e5300cd87a299de4d918bb7ffb1096f7693f12f6
mmetcalf14 <mekena.mcgrew@gmail.com>
2015-07-02 10:21:28 -0700

    Commit to class form

--- Fock Basis Binary/Hamiltonian_Template.h
+++ Fock Basis Binary/Hamiltonian_Template.h
@@ -15,7 +15,75 @@
 #define Fock_Basis_Binary_Hamiltonian_Template_h
  class Basis //declare class for basis creation
 {
+private:  //have to talk to values through constructor function
+    
+
+protected:
+    std::vector<size_t> basis_up;
+    std::vector<size_t> basis_down;
+    std::vector<size_t> index_up;
+    size_t count_up, count_dn;
+    size_t L, Nup, Ndn;
+    //how to dynamically allocate. How to talk to private data?
+public:
+    Basis();
+    Basis(size_t _L, size_t Nup, size_t Ndn);
+    void BuildBasis();
+    inline size_t getNsite()const{return L;};
+    inline void changeNsite(size_t New_L){L = New_L;};
     
 };
 
+//template <class T>
+class Hamiltonian :public Basis //declare class for Hamiltonian matrices
+{
+private:
+    
+    int tbar;
+    
+    typedef Eigen::SparseMatrix<size_t> SpMat;
+
+    typedef Eigen::Triplet<size_t> Tp;
+    
+    std::vector<Tp> TL_up;
+    std::vector<Tp> TL_down;
+//    TL_up.reserve(3); //put this in the function
+//    TL_down.reserve(3);
+    
+    //Declaring Matrices
+    SpMat HopHam_up;//declare dimension in function
+    SpMat HopHam_down;
+public:
+    Hamiltonian( size_t _L, size_t _Nup, size_t _Ndn ):Basis(_L, _Nup, _Ndn){};
+    void Set_tbar(int _tbar);
+    void Set_Mat_Dim();
+    void BuildHopHam_up();
+    void BuildHopHam_dn();
+    
+    void Matrix_Build();
+    
+};
+
+
+//Bitewise functions used on Fock states
+size_t testbit(size_t m, int n)// m -> basis integer, n -> site
+{
+    size_t Eval;
+    Eval = (m & (1 << n));
+    return Eval;
+}
+
+size_t bitclr(size_t m,  int n) // set nth bit to zero
+{
+    size_t Clr_bit;
+    Clr_bit = m & ~(1 << n);
+    return Clr_bit;
+}
+
+size_t ibtset(size_t m,  int n)
+{
+    size_t New_State;
+    New_State = m | (1 << n);
+    return New_State;
+}
 #endif
--- Fock Basis Binary/main.cpp
+++ Fock Basis Binary/main.cpp
@@ -15,10 +15,7 @@
 #include "/usr/local/include/c++/4.9.2/Eigen/Eigenvalues"
 #include "/usr/local/include/c++/4.9.2/Eigen/Sparse"
 #include "/usr/local/include/c++/4.9.2/Eigen/StdVector"
-
-int bittest(int m,  int n);
-int bitclr(int m,  int n);
-int ibtset(int m,  int n);
+#include "Hamiltonian_Template.h"
 
 
 int main(int argc, const char * argv[])
@@ -27,176 +24,30 @@ int main(int argc, const char * argv[])
     using namespace Eigen;
     
     
-    int maxrange_up = 0;
-    int minrange_up = 0;
-    int maxrange_down = 0;
-    int minrange_down = 0;
-    int nbit = 0;
+
     int Nup = 2;
     int Ndown = 1;
     int Nsite = 3;
-    int count_up = 0;
-    int count_down = 0;
-    
     double tbar = 1.0;
     
-    typedef SparseMatrix<double> SpMat;
-    
-    typedef Triplet<double> T;
-    
-    vector<T> TL_up;
-    vector<T> TL_down;
-    
-    TL_up.reserve(3);
-    TL_down.reserve(3);
-    
-    for (int i = 1; i <= Nup; i++)
-    {  minrange_up += pow(2,(i-1));
-        maxrange_up += pow(2,(Nsite-i));
-    }
-    
-    for (int i = 1; i <= Ndown; i++)
-    {  minrange_down += pow(2,(i-1));
-        maxrange_down += pow(2,(Nsite-i));
-    }
-    cout << minrange_down << " " << maxrange_down << endl;
-    vector<int> basis_up;
-    vector<int> basis_down;
-//    SparseVector<int> basis_sig(maxrange);
-   // SparseVector<int> index_sig(maxrange+1);//need to choose if i=3 this is element 4 in vector since
-    //starting index is 0
-    vector<int> index_up (maxrange_up+1);
-    vector<int> index_down (maxrange_down+1);
     
     
-    for (int i = minrange_up; i <= maxrange_up; i++) //create spin up basis and vectors
-    {
-        nbit = 0;
-        for(int j = 0; j < Nsite; j++)
-        {
-            if (bittest(i,j))
-            {
-                nbit++;            
-            }
-        }
-        
-        if (nbit == Nup)
-        {
-            count_up++;
-            basis_up.push_back(i);
-            index_up.at(i) = count_up;
-        }
-        
-    }
+    //Build basis and pass to Hamiltonian class through inheritance
+    Hamiltonian ham(Nsite, Nup, Ndown);
+    
+    //set tbar
+    ham.Set_tbar(tbar);
+    ham.Set_Mat_Dim();
+    ham.BuildHopHam_up();
+    
     
 
     
-    for (int i = minrange_down; i <= maxrange_down; i++) //create spin down basis and index
-    {
-        nbit = 0;
-        for(int j = 0; j < Nsite; j++)
-        {
-            if (bittest(i,j))
-            {
-                nbit++;
-            }
-        }
-        
-        if (nbit == Ndown && Ndown != 0)
-        {
-            count_down++;
-            cout << i<< " " << count_down<< endl;
-            basis_down.push_back(i);
-            index_down.at(i) = count_down;
-        }
-        
-    }
-    
-    int Tot_base = count_up*count_down;
-    
-    
-    //Declaring Matrices
-    SpMat H_up(Tot_base,Tot_base);
-    SpMat H_down(Tot_base,Tot_base);
 
-    
-    //Display Basis and Index vectors
-    for( int cnt=0; cnt < basis_up.size(); cnt++)
-    {
-        cout << basis_up[cnt] << endl;
-    }
-    cout << endl;
-    for( int cnt=0; cnt < index_up.size(); cnt++)
-    {
-        cout << index_up[cnt] << endl; //yet it does work here
-    }
-    
-    
-    //Creation of the off diagonal elements through hopping
-    
-    for(int bs = 0; bs < count_up; bs++)
-    {
-        int p_bas = basis_up[bs];
-        int p_ind = index_up[p_bas];
-        //cout << "We are acting on basis, " << p_bas << " with index, "<< p_ind << endl;
-        for(int i = 0; i < (Nsite-1); i++)
-        {
-            int l_bas = ibtset(bitclr(p_bas,i),i+1);
-            int l_ind = index_up[l_bas];
-            
-            if(l_bas != p_bas && l_ind != 0)
-            { //cout << "The hopping term gives the new state: " << l_bas << " With index: " << l_ind <<endl;
-            
-
-            if(count_down > 0 )
-            {
-                for(int k = 1; k <= count_down; k++)
-                {
-                    cout << "CHecking loop \n";
-                    int r = ((k-1)*count_up) + p_ind;
-                    int s = ((k-1)*count_up) + l_ind;
-                    int val = -tbar * pow(-1,bittest(k,i));
-                    TL_up.push_back(T((r-1),(s-1),val));
-                  
-                }
-            }
-            else
-            {   int val = -tbar;
-                TL_up.push_back(T(p_ind-1,l_ind-1, val ));
-               
-            }
-            }
-        }
-    }
-    
-    H_up.setFromTriplets(TL_up.begin(), TL_up.end());
-
-    cout << "The Hamiltonian for up spin is:" << H_up << endl;
-    
     return 0;
 }
 
-// Function returns TRUE is jth bit of int i is 1. FALSE if 0
-int bittest(int m, int n)// m -> basis integer, n -> site
-{
-    int Eval;
-    Eval = (m & (1 << n));
-    return Eval;
-}
 
-int bitclr(int m,  int n) // set nth bit to zero
-{
-    int Clr_bit;
-    Clr_bit = m & ~(1 << n);
-    return Clr_bit;
-}
-
-int ibtset(int m,  int n)
-{
-    int New_State;
-    New_State = m | (1 << n);
-    return New_State;
-}
 
 
 
