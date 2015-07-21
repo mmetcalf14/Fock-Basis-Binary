a98f1dca9c1554cd7292e000d6749de1ff92d6e4
mmetcalf14 <mekena.mcgrew@gmail.com>
2015-07-21 12:57:52 -0700

    The Hamiltonian is set under all conditions

--- Fock Basis Binary/Hamiltonian_Template.h
+++ Fock Basis Binary/Hamiltonian_Template.h
@@ -8,8 +8,8 @@
 
 #include <iostream>
 #include <vector>
-#include "/usr/local/include/c++/4.9.2/Eigen/Eigen"
-#include "/usr/local/include/c++/4.9.2/Eigen/Sparse"
+#include "/usr/include/Eigen/Eigen"
+#include "/usr/include/Eigen/Sparse"
 
 #ifndef Fock_Basis_Binary_Hamiltonian_Template_h
 #define Fock_Basis_Binary_Hamiltonian_Template_h
@@ -22,6 +22,7 @@ protected:
     std::vector<size_t> basis_up;
     std::vector<size_t> basis_down;
     std::vector<size_t> index_up;
+    std::vector<size_t> index_dn;
     size_t count_up, count_dn;
     size_t L, Nup, Ndn;
     //how to dynamically allocate. How to talk to private data?
@@ -39,31 +40,46 @@ class Hamiltonian :public Basis //declare class for Hamiltonian matrices
 {
 private:
     
-    int tbar;
+    double tbar;
+    double U;
     
-    typedef Eigen::SparseMatrix<size_t> SpMat;
+    int Tot_base;
+    
+    typedef Eigen::SparseMatrix<double> SpMat;
 
-    typedef Eigen::Triplet<size_t> Tp;
+    typedef Eigen::Triplet<double> Tp;
     
     std::vector<Tp> TL_up;
     std::vector<Tp> TL_down;
-//    TL_up.reserve(3); //put this in the function
+    std::vector<Tp> TL_Ubase;
+    //    TL_up.reserve(3); //put this in the function
 //    TL_down.reserve(3);
+    
+    //Interaction Index Matrices
+    Eigen::MatrixXd IndexU_up;
+    Eigen::MatrixXd IndexU_dn;
+    int point_up;
+    int point_dn;
     
     //Declaring Matrices
     SpMat HopHam_up;//declare dimension in function
     SpMat HopHam_down;
+    SpMat Ham_Interact;
+    
 public:
     Hamiltonian( size_t _L, size_t _Nup, size_t _Ndn ):Basis(_L, _Nup, _Ndn){};
-    
     //Hamiltonian Functions
-    inline void Set_tbar(int _tbar){ tbar = _tbar;};
     void Set_Mat_Dim();
     void BuildHopHam_up();
     void BuildHopHam_dn();
+    void Interaction_Index();
+    void Build_Interactions();
+    void BaseInteraction();
     
-    void Matrix_Build();
-    
+    void HopMatrix_Build();
+    void IntMatrix_Build();
+    void Set_Const(double _tbar, double _U);
+    void Save_Ham();//input can be filename from main cpp
 };
 
 
