7ec4b000c0b2ab0231bd9a6557de5bad97135cc7
ChenYen Lai <chengyanlai@gmail.com>
2016-07-21 14:37:29 -0700

    group the matrix-matrix multiplication in Dynamics, this accelerates the program and reduces memory usage.

--- Fock Basis Binary/main_dynamic.cpp
+++ Fock Basis Binary/main_dynamic.cpp
@@ -48,9 +48,9 @@ double GdotG( const VectorXcd &Gc1, const VectorXcd &Gc2);
 
 int main(int argc, const char * argv[])
 {
-    
 
-    
+
+
     int Nup;
     int Ndown;
     int Nsite;
@@ -64,14 +64,14 @@ int main(int argc, const char * argv[])
     int T_f;
     double dt = .01;
     double t_p;
-    
+
     const double h0 = 0.5;
     const double d0 = 0.5;
     const double J0 = 1.;
-    
-    
 
-    
+
+
+
     char output[60];
     char HTFout[60];
     char IntFidOut[60];
@@ -79,7 +79,7 @@ int main(int argc, const char * argv[])
     char PeierlsDensity_up[60];
     char PeierlsDensity_down[60];
     char QPumpDensity[60];
-    
+
     Matrix4d Test_Ham;
     Vector4d Test_Lanczos;
     Test_Ham << 0, -1, 0, 0,
@@ -87,7 +87,7 @@ int main(int argc, const char * argv[])
     0,-1,0,-1,
     0,0,-1,0;
     Test_Lanczos << 0.5,0.5,0.5,0.5;
-    
+
     vector<double> Harm_Trap (Nsite, 0.0);
 
     ifstream ReadFile("ED_J1J2_DataInput.cfg");
@@ -97,7 +97,7 @@ int main(int argc, const char * argv[])
         cout<<"NOT OPEN"<<endl;
         exit (1);
     }
-    
+
     ReadFile >> Nup;
     ReadFile >> Ndown;
     ReadFile >> Nsite;
@@ -111,7 +111,7 @@ int main(int argc, const char * argv[])
     ReadFile >> IntFidOut;
     ReadFile >> PeierlsDensity;
     ReadFile >> QPumpDensity;
-    
+
     cout << Nup << endl;
     cout << Ndown << endl;
     cout << Nsite << endl;
@@ -119,55 +119,55 @@ int main(int argc, const char * argv[])
     cout << t_2 << endl;
     cout << U << endl;
     cout << output << endl;
-    
-    
-    
+
+
+
     int T_tot = T_f/dt;
-    
-    
+
+
     ofstream fout(output);
     assert(fout.is_open());
     fout.setf(ios::scientific);
     fout.precision(11);
-    
+
     ofstream FidOut(IntFidOut);
     assert(FidOut.is_open());
     FidOut.setf(ios::scientific);
     FidOut.precision(11);
-    
+
     ofstream HTOut(HTFout);
     assert(HTOut.is_open());
     HTOut.setf(ios::scientific);
     HTOut.precision(11);
-    
+
     ofstream PDout(PeierlsDensity);
     assert(PDout.is_open());
     PDout.setf(ios::scientific);
     PDout.precision(11);
-    
+
     ofstream QPout(QPumpDensity);
     assert(QPout.is_open());
     QPout.setf(ios::scientific);
     QPout.precision(11);
-    
+
     ofstream PDout_up("ED_J1gtJ2_U10_Nu3_L5_Thouless_TimeEvolved_Density_070516.dat");
     assert(PDout_up.is_open());
     PDout_up.setf(ios::scientific);
     PDout_up.precision(11);
-    
+
     ofstream PDout_dn("ED_J1gtJ2_U10_Nd3_L5_Thouless_TimeEvolved_Density_070516.dat");
     assert(PDout_dn.is_open());
     PDout_dn.setf(ios::scientific);
     PDout_dn.precision(11);
-    
+
    //creating vector with harmoinc trap values per site
     //Harmonic_Trap(Harm_Trap, Nsite, Y);
-    
-    //Build basis and pass to Hamiltonian class through inheritance
-    Hamiltonian<complex<double>> ham(Nsite, Nup, Ndown);
-    Lanczos_Diag<complex<double>> Diag(ham);
 
-    
+    //Build basis and pass to Hamiltonian class through inheritance
+    Hamiltonian<complex<double> > ham(Nsite, Nup, Ndown);
+    Lanczos_Diag<complex<double> > Diag(ham);
+
+
     //ham.GetHarmTrap(Harm_Trap);
     //Fidelity_HT(HTOut, ham, Diag, U, t_1, t_2, Umax, Ymax, Harm_Trap, Nsite);
     //Fidelity(FidOut, ham, Diag, U, t_1, t_2, Umax, Jmax, Nsite);
@@ -175,32 +175,32 @@ int main(int argc, const char * argv[])
 
     //set hopping and interaction coefficients
     ham.Set_Const(t_1, t_2, U);//U=0 until |G> is found for t=0
-    
-    
-    
+
+
+
     //set hamiltonian from triplets
 //    ham.HopMatrix_Build();
-//    
-//    
+//
+//
 //    //build interaction matrix
     ham.IntMatrix_Build();
-//    
+//
 //    //add together all three matrices for total Ham
 //    ham.Total_Ham();
-//    
+//
 //    //create object for diag class
-//    
+//
 //    //Diag.Lanczos_TestM(Test_Ham, Test_Lanczos);
-//    
+//
 //    //set Lanczos vector dimensions
 //    cout << "Setting LA Dim \n";
 //    Diag.Set_Mat_Dim_LA(ham);
-//    
+//
 //    cout << "Diagonalizing \n";
 //    //Diagonalization of t=0 Hamiltonian
 //    Diag.Diagonalize(ham);
-//    
-//    
+//
+//
 //    //convert |G> from Fock basis to onsite basis
 //    //seperate |G> states for nup and ndn
 //    cout << "Getting Density\n";
@@ -210,23 +210,23 @@ int main(int argc, const char * argv[])
         //Triplets removed to redo Interaction matrix after quenching
         // and all non-zero elemenst of Total Ham and Ham_U are set to zero
         //ham.ClearInteractTriplet();
-    
-    
+
+
         //Interactions turned on after intial ground state found
         //if U=0 there is no need to build interaction ham until after ground state is found
 //        ham.QuenchU(U);
 //        ham.IntMatrix_Build();
 //        ham.Total_Ham();
-    
+
         //Lanczos_Diag<complex<double> > Diag_Comp(ham);
-    
+
         //Time Evolve
-    
+
     QPump_TD(QPout, ham, Diag, T_f, dt, h0, J0, d0, U, Nsite);
-       
-    
+
+
         //int NN = T_tot/10;
-    
+
     FidOut.close();
     HTOut.close();
     fout.close();
@@ -234,9 +234,9 @@ int main(int argc, const char * argv[])
     PDout_dn.close();
     PDout_up.close();
     QPout.close();
-    
+
     cout << "Code is Done! \n";
-    
+
     return 0;
 }
 
@@ -245,10 +245,10 @@ void Write_Density(ofstream &fout, vector<double> &n_up, vector<double> &n_dn, i
     for(int i = 0; i < L; i++)
     {
         fout << i << " " << n_up[i] << " " << n_dn[i] << endl;
-        
+
     }
     fout << endl;
-    
+
 }
 
 void DensityDiff(ofstream &fout, vector<double> &n_up, vector<double> &n_dn, int L )
@@ -256,7 +256,7 @@ void DensityDiff(ofstream &fout, vector<double> &n_up, vector<double> &n_dn, int
     for(int i = 0; i < L; i++)
     {
         fout << i << " " << n_up[i] - n_dn[i] << endl;
-        
+
     }
     fout << endl;
 }
@@ -266,9 +266,9 @@ void DensityInTime(ofstream &fout, vector<double> &n, int L, double Phi )
     for(int i = 0; i < L; i++)
     {
         fout << i << " " << Phi << " "  << n[i] << endl;
-        
+
     }
-    
+
 }
 
 void Density(ofstream &fout, vector<double> &n, int L)
@@ -276,7 +276,7 @@ void Density(ofstream &fout, vector<double> &n, int L)
     for(int i = 0; i < L; i++)
     {
         fout << i << " "<< n[i] << endl;
-        
+
     }
     fout << endl;
 }
@@ -286,14 +286,14 @@ void Harmonic_Trap(vector<double> &HT, int L, double y)
     int Ro;
     if(L % 2 == 0)
     {
-        
+
      Ro = (L)/2;
     }
     else
     {
     Ro = (L +1)/2;
     }
-    
+
     //cout << "R0: " << Ro << endl;
     for(int i = 1; i <= L; i++)
     {
@@ -301,8 +301,8 @@ void Harmonic_Trap(vector<double> &HT, int L, double y)
         HT.push_back(val);
         //cout << HT[i-1] << endl;
     }
-    
-    
+
+
 }
 
 template<typename Tnum>
@@ -310,7 +310,7 @@ void Fidelity(ofstream &output, Hamiltonian<Tnum> &h, Lanczos_Diag<Tnum> &d, dou
 
 {
     typedef Eigen::Matrix<Tnum, Eigen::Dynamic, 1> VectorType;
-    
+
     double dU = 1.;
     double dJ = .1;
     int Jtot = Jm/dJ;
@@ -320,13 +320,13 @@ void Fidelity(ofstream &output, Hamiltonian<Tnum> &h, Lanczos_Diag<Tnum> &d, dou
     VectorType Gstate;
     VectorType Gstate_Temp;
    // U = 0;
-    
+
     for(int i = 0; i <= Utot ; i++)
     {
         cout << "Loop: " << i << endl;
         for(int j = 0; j <= Jtot; j++)
         {
-            
+
             h.Set_Const(J1, J2, U);//not having U will be a problem
             h.HopMatrix_Build();
             if(j == 0)
@@ -343,26 +343,26 @@ void Fidelity(ofstream &output, Hamiltonian<Tnum> &h, Lanczos_Diag<Tnum> &d, dou
 
                 g = 2*(1-F)/(L*(dU*dU));//this is wrong
                 output << U << " " << J2 << " " << g << endl;
-                
+
             }
             Gstate_Temp = Gstate;
             J2 += dJ;
             h.ClearHopTriplet();
-            
+
         }
         U += dU;
         J2 = 0;
         h.ClearInteractTriplet();
     }
-    
-    
+
+
 }
 
 template<typename Tnum>
 void U_Fid(ofstream &output, Hamiltonian<Tnum> &h, Lanczos_Diag<Tnum> &d , double U, double J1, double J2, double Um, int L)
 {
     typedef Eigen::Matrix<Tnum, Eigen::Dynamic, 1> VectorType;
-    
+
     double ddU = .0001;
     double dU = 1.;
     double UU;
@@ -373,19 +373,19 @@ void U_Fid(ofstream &output, Hamiltonian<Tnum> &h, Lanczos_Diag<Tnum> &d , doubl
     VectorType Gstate_i;
     VectorType Gstate_f;
     // U = 0;
-    
+
     for(int i = 0; i <= Utot ; i++)
     {
         cout << "Loop: " << i << " U: "<< U<< endl;
 
-            
+
         h.Set_Const(J1, J2, U);//change value of U
-        
+
         if(i == 0)
         {
         h.HopMatrix_Build();//only need to build hopham once
         }
-        
+
         //get preliminary Gstate
             h.IntMatrix_Build();//build new matrix for every iteration
             h.Total_Ham();//combine hopham and hopint
@@ -393,7 +393,7 @@ void U_Fid(ofstream &output, Hamiltonian<Tnum> &h, Lanczos_Diag<Tnum> &d , doubl
             d.Set_Mat_Dim_LA(h);
             d.Diagonalize(h);
             Gstate_i = d.SendGstate();
-        
+
         //add ddu to U and get next G-state
         UU = U+ddU;//increase U
         cout << "U+ddu: "<< UU << endl;
@@ -404,25 +404,25 @@ void U_Fid(ofstream &output, Hamiltonian<Tnum> &h, Lanczos_Diag<Tnum> &d , doubl
         d.Set_Mat_Dim_LA(h);
         d.Diagonalize(h);
         Gstate_f = d.SendGstate();//Gstate of U+ddU
-        
+
         //get fidelity between U and U+ddU
 
             F = GdotG(Gstate_f, Gstate_i);
             g = 2.*(1.-F)/(L*(ddU*ddU));//changing variable is U
         glog = (-(2./L)*log(F))/(ddU*ddU);
             output << U << " " << g << endl;
-        
+
         if(i == 0)
         {
             cout << scientific;
             cout << "F: " << F << " g: " << g << " glog: " << glog <<"Log(F) " << log(F) <<  endl;
         }
-        
+
         //clear interaction matrix for next iteration
         U++;
         h.ClearInteractTriplet();
     }
-    
+
 
 }
 
@@ -430,7 +430,7 @@ template<typename Tnum>
 void Fidelity_HT(ofstream &output, Hamiltonian<Tnum> &h, Lanczos_Diag<Tnum> &d , double U, double J1, double J2, double Um, double Ym, vector<double> HT, int L)
 {
     typedef Eigen::Matrix<Tnum, Eigen::Dynamic, 1> VectorType;
-    
+
     double dU = 1.;
     double dw = .01;
     double y = 0;
@@ -441,7 +441,7 @@ void Fidelity_HT(ofstream &output, Hamiltonian<Tnum> &h, Lanczos_Diag<Tnum> &d ,
     VectorType Gstate;
     VectorType Gstate_Temp;
     // U = 0;
-    
+
 
         for(int w = 0; w <= Wtot; w++)
         {
@@ -470,13 +470,13 @@ void Fidelity_HT(ofstream &output, Hamiltonian<Tnum> &h, Lanczos_Diag<Tnum> &d ,
                 cout << "F: " << F << endl;
                 g = 2*(1-F)/(L*(dw*dw));//this is wrong
                 output << y << " " << g << endl;
-                
+
             }
             Gstate_Temp = Gstate;
             y += dw;
             h.ClearHopTriplet();
             HT.clear();
-            
+
         }
 //        U += dU;
 //        J2 = 0;
@@ -492,33 +492,33 @@ void PeierlsTD(ofstream &out, Hamiltonian<Tnum> &h, Lanczos_Diag<Tnum> &d, int T
     double t = 0.;
     double Phi_max = (4*atan(1.0))/2.;
     double Phi_t = 0.0;
-    
+
     for(int it = 0; it <= T_it; it++)
     {
         t = it*dt;
 
-        
-        
+
+
         if(t <= tp)
         {
             Phi_t = (t/tp)*Phi_max;
             h.GetPhi(Phi_t);
             h.HopMatrix_Build_Peierls();
-            
+
             h.Total_Ham();
             if(it == 0)
             {
                 d.Set_Mat_Dim_LA(h);
                 d.Diagonalize(h);
-                
+
                 d.Density(h);
                 Write_Density(out, d.n_up, d.n_dn, L);
-                
+
             }
             //                else{
             d.Dynamics(h);
             //                }
-            
+
             if(t < tp)
             {
                 h.ClearHopTriplet();
@@ -529,22 +529,22 @@ void PeierlsTD(ofstream &out, Hamiltonian<Tnum> &h, Lanczos_Diag<Tnum> &d, int T
             //ham.OutHam();
             d.Dynamics(h);
         }
-        
-        
-        
+
+
+
         if(Nflag == 100)
         {
             cout << "Getting Density for t=" << t << endl;
             d.Density(h);
             Write_Density(out, d.n_up, d.n_dn, L);
-            
+
             //DensityDiff(PDout, Diag.n_up, Diag.n_dn, Nsite);
             Nflag = 0;
         }
-        
+
         Nflag++;
-        
-        
+
+
     }
 
 }
@@ -556,14 +556,14 @@ void QPump_TD(ofstream &out, Hamiltonian<Tnum> &ham, Lanczos_Diag<Tnum> &diag, c
     double J1, J2;
     double t;
     double h;
-    
+
     diag.TimeEvoCoeff(dt);
     ham.ClearHopTriplet();
-    
+
     int T_it = T /dt;
-    
+
     const double omega = (8*atan(1.0))/T;
-    
+
     for( int it = 0; it <= T_it; it++)
     {
         t = it*dt;
@@ -572,17 +572,17 @@ void QPump_TD(ofstream &out, Hamiltonian<Tnum> &ham, Lanczos_Diag<Tnum> &diag, c
         {
             cout << "t: " << t << endl;
         }
-        
+
         delta = d_0*cos(omega*t);
         J1 = J_0 + delta;
         J2 = J_0 - delta;
         h = h_0*sin(omega*t);
         ham.GetOnsite(h);
-        
+
         ham.Set_Const(J1, J2, U);
         ham.HopMatrix_Build_QPump();
         ham.Total_Ham();
-        //cout << "Have Total Ham\n";
+        // cout << "Have Total Ham" << endl;
         if(it == 0)
         {
             diag.Set_Mat_Dim_LA(ham);
@@ -590,10 +590,10 @@ void QPump_TD(ofstream &out, Hamiltonian<Tnum> &ham, Lanczos_Diag<Tnum> &diag, c
         }
         else
         {
-            //cout << "Beginning Dynamics\n";
+            cout << "Beginning Dynamics" << endl;
             diag.Dynamics(ham);
         }
-        
+
         ham.ClearHopTriplet();
         //cout << "Triplet Clear\n";
         if(it == 0 || it == T_it/2. || it == T_it)
@@ -602,7 +602,7 @@ void QPump_TD(ofstream &out, Hamiltonian<Tnum> &ham, Lanczos_Diag<Tnum> &diag, c
             //cout << "Hamiltonian: \n" << ham.Ham_Tot << endl;
             diag.Density(ham);
             Write_Density(out, diag.n_up, diag.n_dn, L);
-            
+
         }
     }
 }
@@ -611,7 +611,7 @@ double GdotG( const VectorXd &G1, const VectorXd &G2)
 {
     double F;
     F = abs(G1.dot(G2));
-   
+
     //cout << "Fidelity: " << F << endl;
     return F;
 }
@@ -622,11 +622,9 @@ double GdotG( const VectorXcd &Gc1, const VectorXcd &Gc2)
     F = Gc1.dot(Gc2);
     Fp = F*conj(F);
     F = sqrt(Fp);
-    
+
     return F.real();
 }
 
 template class Hamiltonian<double>;
 template class Hamiltonian<complex<double> >;
-
-
