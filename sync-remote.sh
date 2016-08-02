4cf146a707c17e14c1058d85b9ba7fa2c7ca833a
mmetcalf14 <mekena.mcgrew@gmail.com>
2016-08-02 11:41:22 -0700

    New makefile

---
+++ sync-remote.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+rsync -avhe ssh atomtronics:~/Codes/Fock-Basis-Binary/Fock-Basis-Binary/ /Users/mekenametcalf/Dropbox/Topological\ Insulators/Codes/Fock-Basis-Binary/Fock-Basis-Binary/
